69477af165b32cb60d7cacfc74b0c59cc5369d9f
itsDalcaN <dsumindan@oakland.edu>
2021-03-16 17:48:25 -0400

    Hello World

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Art Gallery</title>
+    </head>
+    <body>
+        <h1>Art Gallery Management System</h1>
+        <p>Paragraph innit?</p>
+        
+    </body>
+</html>
+
+<?php 
+echo "PHP Script Here!";
+?>
