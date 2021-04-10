d7a9c3535033a822eafc42a4291bf4109467737d
carllane <clane@oakland.edu>
2021-04-09 23:40:53 -0400

    manageupdate

--- manage.php
+++ manage.php
@@ -16,47 +16,56 @@
         <a class="active" href="manage.php">Manage</a>
     </div>
     <div class="main">
-        <table>
-            <!-- <caption>Artwork</caption> -->
-            <tr>
-                <th>Image</th>
-                <th>Name</th>
-                <th>Artist</th>
-                <th>Year</th>
-                <th>Price</th>
-                <th>Type</th>
-                <th>Movement</th>
-            </tr>
 
 
-            <tr>
-                <td><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/300px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg" alt="Mona Lisa" ></td>
-                <td><i>Mona Lisa</i></td>
-                <td>Leonardo de Vinci</td>
-                <td>1517</td>
-                <td>$100 million</td>
-                <td>Painting</td>
-                <td>Renaissance</td>
-                
-                
-            </tr>
-        
-        </table>
-    </div> 
-    <div class ='btn'>
-    <input type ='button' class = 'but btn' value = 'Edit'>
-    <input type ='button' class = 'but btn' value = 'delete'>
-    </div>
-</body>
-</html>
+    <div class="header">
+    <h1>Employee Login</h1>
+    <?php
+        if (isset($_SESSION['visitor-logged-in'])) {
+            $sql = "SELECT Name FROM visitor WHERE ID=" .$_SESSION['visitor-logged-in'];
+            $result = mysqli_query($link, $sql) or die(mysqli_error($link));
+            $row = mysqli_fetch_array($result);
+            echo "<p style='text-align:center'>Logged in as " . $row['Name']."</p>";
+        } else {
+            echo "<p style='text-align:center;color:#fa2742'><strong>Not Logged In!</strong></p>";
+        }
+
+
+       
+        if (!isset($_SESSION['visitor-logged-in'])) {
+            echo '<a class="button-link" href="login.php" style="position: fixed; top:50px; right:30px">Log In</a>';
+        } else {
+            echo '<a class="button-link" href="includes/logout-inc.php" style="position: fixed; top:50px; right:25px">Log Out</a>';
+        }
+        # Existing search query content
+        if (isset($_SESSION["search-content"])) {
+            $content = $_SESSION["search-content"];
+            if ($content !== false) {
+                echo $content;
+            } else {
+                echo "<h4>No tours found</h4>";
+            }
+        }
+    ?>
+</section>
+</div> 
+<?php include_once 'includes/footer.php'; ?>
+
+    ?>
+</div>
+<div cla
+     
+
+
 
 <?php
-require_once 'connect.php';
-$sql = "SELECT * FROM guide";
-$result = mysqli_query($link, $sql) or die(mysqli_error($link));
+//require_once 'connect.php';
+//$sql = "SELECT * FROM guide";
+//$result = mysqli_query($link, $sql) or die(mysqli_error($link));
 
-echo "<h2>Test</h2><br>";
-while($row = mysqli_fetch_array($result)) {
-    echo "Guide name " . $row['Name'] . "<br>";
-}
-?>
+//echo "<h2>Test</h2><br>";
+//while($row = mysqli_fetch_array($result)) {
+ //   echo "Guide name " . $row['Name'] . "<br>";
+//}
+
+
